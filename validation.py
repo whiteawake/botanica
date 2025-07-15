6f6ece7d44cb07611de084314f4c24c60d359a8c
Asha <asha.amaranti@protonmail.com>
2025-07-15 18:20:24 +0800

    Update validation

--- validation.py
+++ validation.py
@@ -13,10 +13,12 @@ username = form.getvalue('username')
 
 if ((username == 'dbudd') and (password == 'Plant7')):
 	print("<meta http-equiv=\"refresh\" content=\"0; url=/botanica/login/dbudd/\">")
-elif ((username == 'asha') and (password == 'Plant7')):
+elif ((username == 'nsouth') and (password == 'Plant8')):
+	print("<meta http-equiv=\"refresh\" content=\"0; url=/botanica/login/nsouth/\">")
+elif ((username == 'asha') and (password == 'Plant9')):
 	print("<meta http-equiv=\"refresh\" content=\"0; url=/botanica/login/asha/\">")
-elif ((username == 'dblayn') and (password == 'Plant7')):
-	print("<meta http-equiv=\"refresh\" content=\"0; url=/botanica/login/dblayney/\">")
+elif ((username == 'dblayn') and (password == 'Plant10')):
+	print("<meta http-equiv=\"refresh\" content=\"0; url=/botanica/login/dblayn/\">")
 else:
 	print("<script type=\"text/javascript\">window.alert(\"Incorrect username/password!\");</script>"),
 	print("<meta http-equiv=\"refresh\" content=\"0; url=/botanica/\">")
