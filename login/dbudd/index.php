b105a317b3e903a896f972baa7460eb86031dda1
Asha <asha.amaranti@protonmail.com>
2025-07-15 18:24:35 +0800

    Add login files

---
+++ login/dbudd/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Login - Botanica</title>
+	<meta name="author" content="Asha" />
+	<link rel="stylesheet" type="text/css" media="screen" href="login.css" />
+	<link rel="icon"type="image/x-icon" href="../../resources/images/tree.png" />
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<meta http-equiv="refresh" content="7; url=/botanica/welcome.py">
+    	<script type="text/javascript">
+    	$(document).ready(function(){
+  			animate();
+		});
+		function animate(){
+			$(".progress_bar").width("0px");
+			$(".progress_bar").animate({
+				width: '99%'
+			}, 6000, function(){
+				setTimeout('animate()',9900);
+			});
+		}
+	</script>
+</head>
+<body>
+	<link href='http://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
+	<div class="window">
+		<div class="middle">
+		</div>
+	</div>
+	<h1>Mr Budd</h1>
+	<p>Welcome</p>
+	<div id="loader" class="loading">
+		<div class="progress">
+			<div class="progress_bar">
+			</div>
+			<div id="prog_status">
+				Loading...
+			</div>
+		</div>
+	</div>
+</body>
+</html>
